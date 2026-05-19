b2e9d950818adc78567f544ac620939ffd240ab4
brunojdsz <bruno.bjwm@gmail.com>
2026-05-19 10:26:44 -0300

    :construction: building api to connect with chat gpt

---
+++ src/main/java/br/com/brunojdsz/SpringIaBrunojdszApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunojdsz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIaBrunojdszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIaBrunojdszApplication.class, args);
+	}
+
+}
